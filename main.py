ef481df79d1199e9fc5efde89064462abccb29fd
Chezzman <gamingmother@gmail.com>
2018-10-17 15:03:42 +0100

    adding in various attractions for destinations

--- main.py
+++ main.py
@@ -11,4 +11,30 @@ def get_traveler_location(traveler):
     traveler_destination_index = get_destination_index(traveler_destination)
     return traveler_destination_index
 
-print(get_traveler_location(test_traveler))
+#Testing the test Traveler to mak sure basic functionality is there
+#print(get_traveler_location(test_traveler))
+
+attractions = [[] for i in destinations]
+
+def add_attraction(destination, attraction):
+    try:
+        destination_index = get_destination_index(destination)
+        attractions_for_destination = attractions[destination_index].append(attraction)
+
+    except SyntaxError:
+        return
+
+
+add_attraction('Los Angeles, USA', ['Venice Beach', ['beach']])
+add_attraction('Paris, France', ['the Louvre', ['art', 'museum']])
+add_attraction('Paris, France', ['Arc de Triomphe', ['historical site', 'monument']])
+add_attraction('Shanghai, China', ['Yu Garden', ['garden', 'historical site']])
+add_attraction('Shanghai, China', ['Yuz Museum', ['art', 'museum']])
+add_attraction('Shanghai, China', ['Oriental Pearl Tower', ['skyscrapper', 'viewing deck']])
+add_attraction('Los Angeles, USA', ['LACMA', ['art', 'museum']])
+add_attraction('Sao Paulo, Brazil', ['Sao Paulo Zoo', ['zoo']])
+add_attraction('Sao Paulo, Brazil', ['Patio do Colegio', ['historical site']])
+add_attraction('Cairo, Egypt', ['Pyramids of Giza', ['monument', 'historical site']])
+add_attraction('Cairo, Egypt', ['Egyption Museum', ['museum']])
+
+print(attractions)
